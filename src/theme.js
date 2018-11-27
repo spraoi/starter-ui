5d681e041a3565cbf131294d18750c6c4456cdfb
Cade Scroggins <git@cadejs.com>
2018-11-27 00:08:54 -0800

    smarter theme structure

---
+++ src/theme.js
@@ -0,0 +1,10 @@
+// TODO: import spraoiTheme from '@spraoi/theme';
+
+const defaultTheme = {
+  // ...spraoiTheme,
+  // TODO: add addition theme stuff
+};
+
+export default {
+  default: defaultTheme,
+};
