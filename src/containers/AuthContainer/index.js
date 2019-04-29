37ac87b8b77c701c417e63977cdfa1995606bbfa
Cade Scroggins <git@cadejs.com>
2019-04-28 19:15:42 -0700

    properly import parse

--- src/containers/AuthContainer/index.js
+++ src/containers/AuthContainer/index.js
@@ -1,15 +1,15 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import get from 'lodash/get';
-import qs from 'query-string';
 import { AuthContext } from '@spraoi/auth';
 import { Location } from '@reach/router';
+import { parse } from 'query-string';
 import Route from '../../components/Route';
 
 const AuthContainer = ({ children }) => (
   <Location>
     {({ location: { search } }) => (
-      <Route isPublic redirect={get(qs.parse(search), 'redirect', '/')}>
+      <Route isPublic redirect={get(parse(search), 'redirect', '/')}>
         <AuthContext.Consumer>{children}</AuthContext.Consumer>
       </Route>
     )}
