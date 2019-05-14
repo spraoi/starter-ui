3ee55e189e906bd0d02b23d495cad371a67c7345
Cade Scroggins <git@cadejs.com>
2019-05-14 09:33:29 -0700

    no default user creation as it varies by project

--- src/graphql/mutations.js
+++
@@ -1,9 +0,0 @@
-import gql from 'graphql-tag';
-
-export const UPDATE_SELF_MUTATION = gql`
-  mutation($email: String!, $givenName: String!, $familyName: String!) {
-    updateSelf(email: $email, givenName: $givenName, familyName: $familyName) {
-      id
-    }
-  }
-`;
